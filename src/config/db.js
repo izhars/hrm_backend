ecd08751ce13b7df8799bb6606d5dc22943f52a8
Rique <khannsharique007@gmal.com>
2025-11-14 13:23:15 +0530

    Production-ready: secure env, conditional dotenv, .gitignore

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,16 @@
+// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    console.error('MONGODB_URI is not defined!');
+    process.exit(1);
+  }
+
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
