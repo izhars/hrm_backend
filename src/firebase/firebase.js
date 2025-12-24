edd48d406df6198ccf9babfb227ef8a1ae2ddd0a
sharique izhar <khannshariq007@gmail.com>
2025-12-24 12:42:12 +0530

    Initial commit: REST API code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,41 @@
 // utils/firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseKey.json"); // path to your Firebase service account
+require("dotenv").config();
 
-if (!admin.apps.length) { // prevent re-init errors
+const serviceAccount = {
+  project_info: {
+    project_number: process.env.FIREBASE_PROJECT_NUMBER,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    storage_bucket: process.env.FIREBASE_STORAGE_BUCKET
+  },
+  client: [
+    {
+      client_info: {
+        mobilesdk_app_id: process.env.FIREBASE_MOBILESDK_APP_ID,
+        android_client_info: {
+          package_name: process.env.FIREBASE_PACKAGE_NAME
+        }
+      },
+      oauth_client: [],
+      api_key: [
+        {
+          current_key: process.env.FIREBASE_API_KEY
+        }
+      ],
+      services: {
+        appinvite_service: {
+          other_platform_oauth_client: []
+        }
+      }
+    }
+  ],
+  configuration_version: process.env.FIREBASE_CONFIG_VERSION
+};
+
+if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    storageBucket: serviceAccount.project_info.storage_bucket
   });
 }
 
